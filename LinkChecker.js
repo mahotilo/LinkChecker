954da566ee7190ddc5258c1b3e25996318a10f7d
mahotilo <kvmahotilo@gmail.com>
2020-09-06 19:55:44 +0300

    1.4.1

--- LinkChecker.js
+++ LinkChecker.js
@@ -2,7 +2,7 @@
 	
 	ShowWhereLinkIs = function(link){
 		$('.gp_admin_box_close').click();
-		var $el = $('a[href*="'+link+'"]').not($('#gp_admin_html *')).not($('[data-gp_hidden="true"] *')) ;
+		var $el = $('a[href$="'+link+'"]').not($('#gp_admin_html *')).not($('[data-gp_hidden="true"] *')) ;
 		var $elv = $el.add( $el.closest(':visible') );
 
 		$el.add($elv)
@@ -13,7 +13,7 @@
 			next();
 		});
 
-		$("HTML, BODY").animate({scrollTop: $elv.offset().top-100},500);
+		$("HTML, BODY").animate({scrollTop: $elv.offset().top-150},500);
 	}
 	
 })(jQuery);
--- README.md
+++ README.md
@@ -31,6 +31,9 @@ GPL 2, for bundled thirdparty components see the respective subdirectories.
 Based on  https://github.com/jakeRPowell/linkChecker
 
 ## Version history
+1.4.1
+- improve link location
+
 1.4
 - drop tablesort.js and use TS integrated tablesorter.js and classes.
 
